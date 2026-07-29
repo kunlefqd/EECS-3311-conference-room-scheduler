8baaa960395acec69a0d2b05ba983521bfb3bd37
roleyrogan <ronans.gamboa@gmail.com>
2026-07-29 14:08:14 -0400

    Add Test Files For Factory Classes

---
+++ src/main/java/test/java/FacultyFactoryTest.java
@@ -0,0 +1,88 @@
+package test.java;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.conferenceroomscheduler.model.Account;
+import com.conferenceroomscheduler.model.FacultyAccount;
+import com.conferenceroomscheduler.patterns.AccountFactory;
+import com.conferenceroomscheduler.patterns.FacultyFactory;
+
+public class FacultyFactoryTest {
+
+    private FacultyFactory factory;
+
+    @Before
+    public void setUp() {
+        factory = new FacultyFactory();
+    }
+
+    @Test
+    public void testCreateAccountNotNull() {
+        assertNotNull(factory.createAccount("1","a","p","student",true,true,"100"));
+    }
+
+    @Test
+    public void testReturnsFacultyAccount() {
+        Account account = factory.createAccount("1","a","p","student",true,true,"100");
+        assertTrue(account instanceof FacultyAccount);
+    }
+
+    @Test
+    public void testAccountId() {
+        Account account = factory.createAccount("123","a","p","student",true,true,"100");
+        assertEquals("123", account.getAccountId());
+    }
+
+    @Test
+    public void testEmail() {
+        Account account = factory.createAccount("1","abc@test.com","p","student",true,true,"100");
+        assertEquals("abc@test.com", account.getEmail());
+    }
+
+    @Test
+    public void testPassword() {
+        Account account = factory.createAccount("1","a","secret","student",true,true,"100");
+        assertEquals("secret", account.getPassword());
+    }
+
+    @Test
+    public void testAccountTypeAlwaysFaculty() {
+        Account account = factory.createAccount("1","a","p","student",true,true,"100");
+        assertEquals("faculty", account.getAccountType());
+    }
+
+    @Test
+    public void testUniversityAccount() {
+        Account account = factory.createAccount("1","a","p","x",true,true,"100");
+        assertTrue(account.isUniversityAccount());
+    }
+
+    @Test
+    public void testVerified() {
+        Account account = factory.createAccount("1","a","p","x",true,false,"100");
+        assertFalse(account.isVerified());
+    }
+
+    @Test
+    public void testAccountNumber() {
+        Account account = factory.createAccount("1","a","p","x",true,true,"555");
+        assertEquals("555", account.getAccountNumber());
+    }
+
+    @Test
+    public void testBadgeCreated() {
+        Account account = factory.createAccount("1","a","p","x",true,true,"555");
+        assertNotNull(account.getBadge());
+    }
+
+    @Test
+    public void testExtendsAccountFactory() {
+        assertTrue(factory instanceof AccountFactory);
+    }
+}
